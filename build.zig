96a64eec77978c6c90ece5203fb2a8bad6018af9
D-Berg <danielfjberg@gmail.com>
2025-10-19 10:14:15 +0200

    initial commit

---
+++ build.zig
@@ -0,0 +1,23 @@
+const std = @import("std");
+
+const manifest = @import("build.zig.zon");
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = @tagName(manifest.name),
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
+    });
+    b.installArtifact(exe);
+
+    const run_exe = b.addRunArtifact(exe);
+    if (b.args) |args| run_exe.addArgs(args);
+
+    const run_step = b.step("run", "run executable");
+    run_step.dependOn(&run_exe.step);
+}
